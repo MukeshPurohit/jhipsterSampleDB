4ecccf95f25d82e8a62a5dd7862e4dc33a17d0ce
MukeshPurohit <purohit.mukesh@gmail.com>
2017-11-17 08:43:08 +0000

    Add files via upload

---
+++ users_authorities.sql
@@ -0,0 +1,5 @@
+insert into jhi_user_authority (user_id,authority_name) values (1,"ROLE_ADMIN");
+insert into jhi_user_authority (user_id,authority_name) values (1,"ROLE_USER");
+insert into jhi_user_authority (user_id,authority_name) values (3,"ROLE_ADMIN");
+insert into jhi_user_authority (user_id,authority_name) values (3,"ROLE_USER");
+insert into jhi_user_authority (user_id,authority_name) values (4,"ROLE_USER");
